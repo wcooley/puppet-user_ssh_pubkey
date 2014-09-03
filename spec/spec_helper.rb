065be7998088c0a94fc16e63ec2bde2a2d1ea1cb
Wil Cooley <wcooley@nakedape.cc>
2014-09-03 16:29:21 -0700

    Add various bits of config for spec testing facts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,27 @@
+require 'rspec'
+# This has to be before the puppetlabs_spec_helper to be effective. I do not
+# understand why.
+RSpec.configure { |c| c.mock_with :rspec }
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+# From puppetlabs-stdlib/spec/spec_helper.rb
+RSpec.configure do |config|
+  config.before(:all) do
+    # Without this, Facter finds the user_ssh_pubkey set in my
+    # ~/.facter/facts.d/
+    Facter::Util::Config.external_facts_dirs = []
+  end
+
+  config.before(:each) do
+    # Ensure that we don't accidentally cache facts and environment between
+    # test cases.  This requires each example group to explicitly load the
+    # facts being exercised with something like
+    # Facter.collection.loader.load(:ipaddress)
+    Facter::Util::Loader.any_instance.stub(:load_all)
+  end
+
+  config.after :each do
+    Facter.clear
+    Facter.clear_messages
+  end
+end
